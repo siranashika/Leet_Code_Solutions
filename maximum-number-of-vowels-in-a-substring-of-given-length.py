3612a23bbee97ac4e3cb10c24f54cb76bcec081c
ASHIKA <siranashika07_bai28@mepcoeng.ac.in>
2026-03-31 20:57:58 +0530

    Create maximum-number-of-vowels-in-a-substring-of-given-length.py

---
+++ maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        current_vowels = 0        
+        for i in range(k):
+            if s[i] in vowels:
+                current_vowels += 1       
+        max_vowels = current_vowels        
+        for i in range(k, len(s)):
+            if s[i] in vowels:
+                current_vowels += 1
+            if s[i - k] in vowels:
+                current_vowels -= 1         
+            if current_vowels > max_vowels:
+                max_vowels = current_vowels                
+        return max_vowels
