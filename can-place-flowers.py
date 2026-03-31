e1ff6f7d7ab2b1dd00c2e1b69d56ffffb268e714
ASHIKA <siranashika07_bai28@mepcoeng.ac.in>
2026-03-31 20:25:07 +0530

    Create can-place-flowers.py

---
+++ can-place-flowers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        count = 0
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                empty_left = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)                
+                if empty_left and empty_right:
+                    flowerbed[i] = 1
+                    count += 1                    
+        return count >= n
